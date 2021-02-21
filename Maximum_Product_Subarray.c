d2996932556a50fdf77df3d7800ef147f2fb5df0
SuprajaGadikota <79374813+SuprajaGadikota@users.noreply.github.com>
2021-02-21 19:58:59 +0530

    Suntek assignment 1 - strings

---
+++ Maximum_Product_Subarray.c
@@ -0,0 +1,17 @@
+
+int maxProduct(int* nums, int numsSize){
+    int max = nums[0],product;
+    for (int i=0;i<numsSize;i++) {
+           product = nums[i];
+        if (product >= max) {
+            max = product;
+            for (int j = i+1; j<numsSize;j++) {
+                product =product*nums[j];
+                if(product >= max) {
+                    max =product;
+                }
+            }
+        }
+    }
+    return max;
+}
