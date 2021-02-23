8052f55ae85931e0379aadb681009d8a26d1a046
SuprajaGadikota <79374813+SuprajaGadikota@users.noreply.github.com>
2021-02-23 19:18:00 +0530

    Update Search_In_Rotated_Sorted_Array.c

--- Search_In_Rotated_Sorted_Array.c
+++ Search_In_Rotated_Sorted_Array.c
@@ -1,5 +1,4 @@
-#include<stdio.h>
-int main() {
+
 int search(int* nums, int numsSize, int target){
     int mid,low,high;
     low =0;
@@ -25,5 +24,4 @@ int search(int* nums, int numsSize, int target){
         }
     }
     return -1;
-}
 }
